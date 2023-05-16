0028c0922dfab69b92ff9d059f26acd3bc6bc39d
radialserial <ntnbinary@gmail.com>
2023-05-16 18:53:07 -0300

    Tweaked gas page

--- scripts/gasolina.js
+++ scripts/gasolina.js
@@ -4,12 +4,12 @@ function melhor() {
     etanol = parseFloat(frdados.txetanol.value.replace(",", "."))
     gasolina = parseFloat(frdados.txgasolina.value.replace(",", "."))
     if (etanol < 0.7 * gasolina) {
-        document.getElementById("imagem").src = "imagens/etanol.png"
+        document.getElementById("imagem").src = "/assets/images/etanol.jpg"
     } else {
-        document.getElementById("imagem").src = "imagens/gasolina.png"
+        document.getElementById("imagem").src = "/assets/images/gasolina.jpg"
     }
 }
 
 function limpar() {
-    document.getElementById("imagem").src = "imagens/normal.png"
+    document.getElementById("imagem").src = "/assets/images/normal.jpg"
 }
